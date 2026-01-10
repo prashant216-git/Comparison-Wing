b22b6e46bf71cbc959a161984480933d92e73604
prashant <saxenaprashant216@gmail.com>
2026-01-10 11:31:25 +0530

    ok dekhte hai disable krdiya

--- Backend/src/main/java/flightcompare/config/SecurityConfig.java
+++ Backend/src/main/java/flightcompare/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 	protected SecurityFilterChain securityfilterchain(HttpSecurity http) throws Exception{
 	
 		 http
-
+				 .cors().disable()
 		.csrf(csrf->csrf.disable())
 		.authorizeHttpRequests(request->request
 				.requestMatchers("/auth/**",
